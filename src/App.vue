152e958cd0dd97a19ab479f2503afe9be596ed51
amar <kharwadeamar@gmail.com>
2020-01-13 16:27:19 +0530

    vue demo application BMI Calculator 1st commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app" style=" background-image: linear-gradient(#8e44ad,#007991);" class="p-4">
+    
+    <bmiCalculator/>
+  </div>
+</template>
+
+<script>
+import bmiCalculator from './components/bmiCalculator.vue'
+
+export default {
+  name: 'app',
+  components: {
+    bmiCalculator
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/bmiCalculator.vue
@@ -0,0 +1,62 @@
+<template>
+   <div class="card " style="width: 25rem; margin-left:25rem;">
+        <div class="card-body">
+            <h5 class="card-title text-muted" >BMI Calculator</h5>
+                <div class="form-group text-left">
+                    <label for="exampleInputEmail1">Enter Weight</label>
+                    <input type="text" class="form-control" v-model="height" aria-describedby="emailHelp" placeholder="Enter Height in cm">
+                </div>
+                <div class="form-group text-left">
+                    <label for="exampleInputEmail1">Enter Height </label>
+                    <input type="text" class="form-control" v-model="weight" aria-describedby="emailHelp" placeholder="Enter Weight in cm">
+                </div>
+                <div class="form-group text-center">
+                    <button type="submit" class="btn btn-danger" @click="Calculate();" >Calculate BMI</button>
+                </div>
+                <div class="mt-5">
+                    <VueSpeedometer needleColor="black"  needleTransition="easeElastic" needleTransitionDuration="3000" :value="+bmi" :segments="4" style="height: 210px;"   :customSegmentStops="[0,25,50, 75, 100]" :segmentColors='["firebrick","tomato", "gold", "limegreen"]'  :maxValue="100" />
+                    <p v-if="bmi > 0">{{ msg }}</p>
+                </div>
+        </div>
+    </div>
+</template>
+<script>
+import VueSpeedometer from "vue-speedometer";
+
+export default {
+    components: { 
+        VueSpeedometer 
+    }, 
+    template: `<vue-speedometer maxSegmentLabels="12"
+        />`,
+    data(){
+        return{
+            weight:'',
+            height:'',
+            bmi:null,
+            msg:''
+        }
+    },
+    methods:{
+        Calculate(){
+            var w = parseInt(this.weight);
+            var h = parseInt(this.height)/100;
+            this.bmi = w / (h * h);
+            // alert(w+' '+h+' '+bmi);
+            if(this.bmi <= 18.5) {
+                this.msg="You are too thin. \n\nLow: " + this.bmi.toFixed(2) + 'kg/m2';
+                 alert(this.msg);
+             }
+             else if (this.bmi >18.5 && this.bmi <=25 ) {
+                this.msg="You are healthy. \n\nModerate:" + this.bmi.toFixed(2) + 'kg/m2';
+                alert(this.msg );
+            }
+            else if (this.bmi >=25 ) {
+                this.msg="You have overweight. \n\nHigh: " + this.bmi.toFixed(2) + 'kg/m2';
+                alert(this.msg);
+            }
+            // alert(this.bmi);
+        }
+    }
+}
+</script>
