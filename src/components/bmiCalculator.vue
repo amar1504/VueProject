e6fa29c2416510113addea41260cdcd16805c02f
amar <kharwadeamar@gmail.com>
2020-01-13 17:36:45 +0530

    vue demo application BMI Calculator 2nd commit

--- src/components/bmiCalculator.vue
+++ src/components/bmiCalculator.vue
@@ -1,22 +1,23 @@
 <template>
    <div class="card " style="width: 25rem; margin-left:25rem;">
         <div class="card-body">
-            <h5 class="card-title text-muted" >BMI Calculator</h5>
-                <div class="form-group text-left">
+            <h5 class="card-title" >BMI Calculator</h5><hr/>
+                <div class="form-group text-left text-muted">
                     <label for="exampleInputEmail1">Enter Weight</label>
                     <input type="text" class="form-control" v-model="height" aria-describedby="emailHelp" placeholder="Enter Height in cm">
                 </div>
-                <div class="form-group text-left">
+                <div class="form-group text-left text-muted">
                     <label for="exampleInputEmail1">Enter Height </label>
                     <input type="text" class="form-control" v-model="weight" aria-describedby="emailHelp" placeholder="Enter Weight in cm">
                 </div>
                 <div class="form-group text-center">
-                    <button type="submit" class="btn btn-danger" @click="Calculate();" >Calculate BMI</button>
+                    <button type="submit" class="btn btn-block btn-success" @click="Calculate();" >Calculate BMI</button>
                 </div>
                 <div class="mt-5">
-                    <VueSpeedometer needleColor="black"  needleTransition="easeElastic" needleTransitionDuration="3000" :value="+bmi" :segments="4" style="height: 210px;"   :customSegmentStops="[0,25,50, 75, 100]" :segmentColors='["firebrick","tomato", "gold", "limegreen"]'  :maxValue="100" />
+                    <VueSpeedometer needleColor="black" needleTransitionDuration="1000" :value="+bmi" :segments="4" style="height: 210px;"   :customSegmentStops="[0,25,50, 75, 100]" :segmentColors='["firebrick","tomato", "gold", "limegreen"]'  :maxValue="100" />
                     <p v-if="bmi > 0">{{ msg }}</p>
                 </div>
+               
         </div>
     </div>
 </template>
@@ -42,18 +43,26 @@ export default {
             var w = parseInt(this.weight);
             var h = parseInt(this.height)/100;
             this.bmi = w / (h * h);
+            if(this.bmi >=100){
+                this.bmi=100;
+            }else if(this.bmi <0){
+                this.bmi=0;
+            }
             // alert(w+' '+h+' '+bmi);
             if(this.bmi <= 18.5) {
                 this.msg="You are too thin. \n\nLow: " + this.bmi.toFixed(2) + 'kg/m2';
-                 alert(this.msg);
+                this.$alert(this.msg);
+                // alert(this.msg);
              }
              else if (this.bmi >18.5 && this.bmi <=25 ) {
                 this.msg="You are healthy. \n\nModerate:" + this.bmi.toFixed(2) + 'kg/m2';
-                alert(this.msg );
+                this.$alert(this.msg);
+                // alert(this.msg );
             }
             else if (this.bmi >=25 ) {
                 this.msg="You have overweight. \n\nHigh: " + this.bmi.toFixed(2) + 'kg/m2';
-                alert(this.msg);
+                this.$alert(this.msg);
+                // alert(this.msg);
             }
             // alert(this.bmi);
         }
--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import VueSimpleAlert from "vue-simple-alert";
+
+Vue.use(VueSimpleAlert);
+
 Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
